64c819e7fa0d86a6c4b653185563c7eac81938f0
Visvaldas <visva.rapalis@gmail.com>
2020-09-08 15:03:54 +0300

    Add users to posts connection

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_08_093321) do
+ActiveRecord::Schema.define(version: 2020_09_08_115140) do
 
   create_table "posts", force: :cascade do |t|
     t.text "post"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
